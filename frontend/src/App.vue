b891726af6602ad125fcef98b3b7cbc2edc16df3
Luis-devs <luismonteria2001@gmail.com>
2024-08-16 18:25:28 -0500

    Proyecto inicializado

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    API: import.meta.env.VITE_APP_API_URL,
+  }),
+  async created() {
+    //Cargar sesiones
+    await axios.put(`${this.API}/sesion/llenar`);
+  },
+};
+</script>
